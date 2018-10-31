dfccdd8e1223f0b42f34c6603e118419854772a9
Alcord <carlitox_free@hotmail.com>
2018-10-31 08:17:59 -0500

    Promesa Append
    Creacion de la promesa Append.
    Falta hacer correr

--- 01-Javascript/07-promesas.js
+++ 01-Javascript/07-promesas.js
@@ -1,7 +1,7 @@
 // 07-promesas.js
 const fs = require('fs');
 
-const nuevaPromesaDeLectura = new Promise(
+/*const nuevaPromesaDeLectura = new Promise(
     (resolve , reject)=>{
         fs.readFile('06-texto.txt','utf-8',
             (err,contenidoArchivo)=>{
@@ -46,21 +46,62 @@ nuevaPromesaDeLectura.then(
     (resultadoError)=>{
         console.log('Algo malo paso',resultadoError);
     }
-);
+);*/
 
 const appendFile = (nombreArchivo, newTexto) => {return new Promise(
     (resolve , reject)=>{
         fs.readFile(nombreArchivo,'utf-8',
             (err,contenidoLeido)=>{
                 if(err){
-                    reject(err,newTexto);
+                    const parametrosReject = {
+                        falla: err,
+                        archivo: nombreArchivo,
+                        nuevoTexto: newTexto
+                    }
+                    reject(parametros);
                 }else{
-                    resolve(contenidoLeido,newTexto);
+                    const parametrosSolve = {
+                        archivo: nombreArchivo,
+                        contenido: contenidoLeido,
+                        nuevoTexto: newTexto
+                    }
+                    resolve(parametrosSolve);
                 }
             })
     }
 )};
 
-appendFile().then(
 
-);
+appendFile()
+    .then(
+        (paramtrosSolve) => {
+            fs.writeFile(
+                paramtrosSolve.archivo,
+                paramtrosSolve.contenido + paramtrosSolve.nuevoTexto,
+                (err) => {
+                    if (err) {
+                        console.error('Error escribiendo');
+                        callback(err);
+                    } else {
+                        console.log('Archivo creado');
+                        //callback(undefined, contenidoArchivoLeido + contenidoArchivo);
+                    }
+                }
+            )
+        }
+)
+    .catch(
+        (parametrosReject) => {
+            fs.writeFile(parametrosReject.archivo, parametrosReject.nuevoTexto,
+                (err) => {
+                    if (err) {
+                        console.error('Error escribiendo');
+                        callback(err);
+                    } else {
+                        console.log('Archivo creado');
+                        //callback(undefined, contenidoArchivo);
+                    }
+                }
+                )
+        }
+    );
