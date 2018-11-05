a45b60c69f6a95c74fb444f20ab6c37a35481c57
Kevin <carlitox_free@hotmail.com>
2018-11-05 09:58:24 -0500

    observable.js

---
+++ 02-typescript/02-observables.js
@@ -0,0 +1,44 @@
+// 02-observables.ts
+// import { PaqueteUno, Paquete dos} from 'rxjs';
+// import * as rxjs from 'rxjs';
+// import {Observable} from "rxjs";
+// declare var module:any;
+const rxjs = require('rxjs');
+const map = require('rxjs/operators').map;
+const disctinct = require('rxjs/operators').distinct;
+const observableUno$ = rxjs.of([1, 2, 3], 3, 'Hola', 3, true, 3, { nombre: 'Adrian' }, new Date(), 3);
+console.log(observableUno$);
+observableUno$
+    .pipe(disctinct(), map((valor) => {
+        console.log('Valor', valor);
+        return {
+            data: valor
+        };
+    }))
+    .pipe()
+    .pipe()
+    .subscribe((ok) => {
+        console.log('En ok', ok);
+    }, (error) => {
+        console.log(error);
+    }, () => {
+        console.log('Completado');
+    });
+const promesita = () => {
+    // @ts-ignore
+    return new Promise((resolve, reject) => {
+        reject(':(');
+    });
+};
+const observableDePromesa$ = rxjs.from(promesita());
+observableDePromesa$
+    .pipe(map((valor) => {
+        return {
+            data: valor
+        };
+    }))
+    .subscribe((objetoFeliz) => {
+        console.log(objetoFeliz);
+    }, (error) => {
+        console.log(error);
+    });
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -2,12 +2,46 @@
 // import { PaqueteUno, Paquete dos} from 'rxjs';
 // import * as rxjs from 'rxjs';
 // import {Observable} from "rxjs";
+
 declare var require: any;
 // declare var module:any;
+
 const rxjs = require('rxjs');
-const observableUno$ = rxjs.of(1, 2, 3, 4, 5, 6, 7);
+const map = require('rxjs/operators').map;
+const disctinct = require('rxjs/operators').distinct;
+
+const observableUno$ = rxjs.of(
+    [1, 2, 3],
+    3,
+    'Hola',
+    3,
+    true,
+    3,
+    {nombre: 'Adrian'},
+    new Date(),
+    3,
+);
+
 console.log(observableUno$);
+
 observableUno$
+    .pipe(
+        disctinct(),
+        map(
+            (valor) => {
+                console.log('Valor', valor);
+                return {
+                    data: valor
+                };
+            }
+        )
+    )
+    .pipe(
+
+    )
+    .pipe(
+
+    )
     .subscribe(
         (ok) => {
             console.log('En ok', ok);
@@ -17,5 +51,35 @@ observableUno$
         },
         () => {
             console.log('Completado');
+        }
+    );
+
+
+const promesita = () => {
+    // @ts-ignore
+    return new Promise(
+        (resolve, reject) => {
+            reject(':(');
+        }
+    )
+};
+const observableDePromesa$ = rxjs.from(promesita());
+
+observableDePromesa$
+    .pipe(
+        map(
+            (valor) => {
+                return {
+                    data: valor
+                }
+            }
+        )
+    )
+    .subscribe(
+        (objetoFeliz) => {
+            console.log(objetoFeliz);
+        },
+        (error) => {
+            console.log(error);
         }
     );
